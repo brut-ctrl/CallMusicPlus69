2f3cb11188b4a6355601873a4599c734778726d8
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-08-28 17:40:06 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ from helpers.modhelps import fetch_heroku_git_url
 load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "BQBW611u38-5R86xZ_xxjwl4ge4MJyevKAOea2_E6m7I_XAM6iE1tL70rlprNHCs5Zaun4sUSOVkqxeu9Mr93JWV8X9BVbH3cUBTwOdH2O-pgeaUSP-U2tAbQEInLvzjEjjAq3Ov9W-0SHIc5I7naYr_CCDERZBtHaOyBh1prPG-EqE7hYj4Z-69QEiEd2klI0vzXYZNy59GZX8G_fVmU-AWAurPEU_ORKr6ZnbG1S6Io0-_hGx_1RmLERAVzj2yb8NxIWWTX9eEvmzOR6F1xGxt0uUs0jisu1revi_ILQm3G_fifXfzy63QV_qSNzcQ12mLR-hLCz9VPUcSoYpBFJGJbIeuqQA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1941313517:AAFsKQ46fS8BgUz1bjDFAo8dmO_62r_UQdI")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
-API_ID = int(getenv("API_ID"))
+API_ID = int(getenv("API_ID", "7341107")
 API_HASH = getenv("API_HASH", "881759527a6576b21c5fc632c2472de3")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
@@ -20,13 +20,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 # Your Telegram User ID
-BOT_OWNER = int(os.environ.get("BOT_OWNER", "1408440765"))
+BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 # Sudo users IDs, They are admins everywhere
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "712147852 1408440765 1944787421 1820831401 1945910995 1909021805 1605366945").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 # Your Bot's Username without "@"
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "CallMusicPlusBot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # Your MongoDB url
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://brut69:HWxGlBegyXXq62Jv@cluster0.l50db.mongodb.net/brut69@admin?retryWrites=true&w=majority")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 # Your Log Channel! Make a private channel and get it's ID
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
 # If you need to broadcast messages as a copy or Forwarded Message
@@ -37,7 +37,7 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://telegra.ph/file/1abf425f2015c0f
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "dunottagme")
 
 # Your ARQ API Key
-ARQ_API_KEY = getenv("ARQ_API_KEY", "JKKWZI-AKBCEI-XIROOB-XQNIZI-ARQ")
+ARQ_API_KEY = getenv("ARQ_API_KEY")
 # Don't Change Anything Here
 ARQ_API_URL = "https://thearq.tech/"
 
