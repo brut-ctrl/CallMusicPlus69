b239de5ae49845b2ced8e275b2301ba025b4d12d
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-08-30 01:26:40 +0700

    Add files via upload

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -5,24 +5,31 @@ from youtube_dl import YoutubeDL
 from config import DURATION_LIMIT
 from helpers.errors import DurationLimitError
 
-ytdl = YoutubeDL(
-    {
-        "format": "bestaudio/best",
-        "geo-bypass": True,
-        "nocheckcertificate": True,
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-    }
-)
+ydl_opts = {
+    "format": "bestaudio/best",
+    "verbose": True,
+    "addmetadata": True,
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+
+ydl = YoutubeDL(ydl_opts)
 
 
 def download(url: str) -> str:
-    info = ytdl.extract_info(url, False)
+    global ydl
+    info = ydl.extract_info(url, False)
     duration = round(info["duration"] / 60)
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"Sorry Bro! Videos longer than {DURATION_LIMIT} minute(s) aren’t allowed, the provided video is {duration} minute(s) . Donate to increase Time Limit"
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
-
-    ytdl.download([url])
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+        )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
